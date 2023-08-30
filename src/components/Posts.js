e715d70d5187d811c6a7641cae046fbb4042d211
Pub <96320073+b3Rhunter@users.noreply.github.com>
2023-08-30 13:05:55 -0500

    Add files via upload

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,10 +1,28 @@
 import React, { useEffect, useState } from "react";
-import { collection, onSnapshot } from "firebase/firestore";
+import { collection, onSnapshot, query, where } from "firebase/firestore"; // Import query and where
 import { db } from "../firebase";
+import NewComment from './NewComment';
+import { FaRegCommentDots } from 'react-icons/fa';
+import { AiFillHeart } from 'react-icons/ai';
 
-function Posts({state, allUserTokens = []}) {
+
+function Posts({ state, allUserTokens = [] }) {
 
   const [posts, setPosts] = useState([]);
+  const [showComments, setShowComments] = useState(false);
+  const [comments, setComments] = useState({});
+
+  const fetchComments = (postId) => {
+    const commentsQuery = query(collection(db, 'comments'), where('postId', '==', postId)); // Use query
+    onSnapshot(commentsQuery, (snapshot) => {
+      const postComments = [];
+      snapshot.forEach((doc) => postComments.push(doc.data()));
+      setComments((prevComments) => ({
+        ...prevComments,
+        [postId]: postComments,
+      }));
+    });
+  };
 
   useEffect(() => {
     const unsubscribe = onSnapshot(collection(db, "posts"), (snapshot) => {
@@ -33,8 +51,7 @@ function Posts({state, allUserTokens = []}) {
     });
     return filtered.sort((a, b) => b.timestamp?.seconds - a.timestamp?.seconds);
   };
-  
-  
+
   const filteredPosts = filterPostsByTokenHolders();
 
   const renderDescriptionWithImages = (description) => {
@@ -44,31 +61,61 @@ function Posts({state, allUserTokens = []}) {
     });
   };
 
-  return(
-      <div className="posts-container">
-{filteredPosts.map((post) => {
-  const nft = findNFTByUsername(post.username);
+  const toggleComments = (postId) => {
+    if (!comments[postId]) {
+      fetchComments(postId);
+    }
+    setShowComments((prevState) => ({
+      ...prevState,
+      [postId]: !prevState[postId],
+    }));
+  };
+
+
   return (
-    <div className="frens-posts" key={post.id}>
-      {nft && (
-        <>
-        <div className="user-post">
-          <img style={{width: "50px", height: "50px"}} className='avatar' src={nft.tokenURI} alt={nft.tokenName} />
-          <h3 style={{fontSize: "18px"}}>{nft.tokenName}</h3>
+    <div className="posts-container">
+      {filteredPosts.map((post) => {
+        const nft = findNFTByUsername(post.username);
+        return (
+          <div className="frens-posts" key={post.id}>
+            {nft && (
+              <>
+                <div className="user-post">
+                  <img style={{ width: "50px", height: "50px" }} className='avatar' src={nft.tokenURI} alt={nft.tokenName} />
+                  <h3 style={{ fontSize: "18px" }}>{nft.tokenName}</h3>
+                </div>
+              </>
+            )}
+            <div className="post-content">
+              <p dangerouslySetInnerHTML={{ __html: renderDescriptionWithImages(post.description) }}></p>
+              {post.image && <img className="postImage" src={post.image} alt="user post" />}
+            </div>
+            <div className="react-btns"> 
+            <button className="open-comments" onClick={() => toggleComments(post.id)}><FaRegCommentDots/></button>
+            <button className="open-comments"><AiFillHeart/></button>
+            </div>
+            {showComments[post.id] && (
+              <div className="comment-section">
+                <NewComment
+                  postId={post.id}
+                  userId={state.userDetails?.id || 'defaultUserId'} // Replace with actual user ID
+                  username={state.userDetails?.name || 'defaultUsername'} // Replace with actual username
+                />
+
+                <div className="existing-comments">
+                  {comments[post.id]?.map((comment, index) => (
+                    <div key={index}>
+                      <p><strong>{comment.username}</strong>: {comment.text}</p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
         </div>
-        </>
-      )}
-      <div className="post-content">
-        <p dangerouslySetInnerHTML={{ __html: renderDescriptionWithImages(post.description) }}></p>
-        {post.image && <img className="postImage" src={post.image} alt="user post" />}
-      </div>
+      );
+    })}
     </div>
   );
-})}
-
-      </div>
-  );
 }
-
 
 export default Posts;
